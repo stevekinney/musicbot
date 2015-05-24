08f4e29128ea6b62119596855f62841a76e438a9
Steve Kinney <hello@stevekinney.net>
2015-05-24 12:24:13 -0600

    Get basic server-side implementation working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 const path = require('path');
 
@@ -16,7 +17,6 @@ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-var port = process.env.PORT || 3000;
 http.listen(port, function(){
   console.log('Your server is up and running on Port ' + port + '. Good job!');
 });
--- public/application.js
+++ public/application.js
@@ -9,12 +9,12 @@ var outputs = {
 
 socket.on('setup', function (data) {
   console.log(data);
-  outputs.buttonTwo.text(data.buttonTwo);
-  outputs.buttonFour.text(data.buttonFour);
-  outputs.buttonSix.text(data.buttonSix);
+  outputs.buttonTwo.text(data.buttonTwo ? 'pressed' : 'released');
+  outputs.buttonFour.text(data.buttonFour ? 'pressed' : 'released');
+  outputs.buttonSix.text(data.buttonSix ? 'pressed' : 'released');
   outputs.potentiometer.text(data.potentiometer);
 });
 
 socket.on('johnny-five', function (data) {
-  outputs[data.input].text(data.value);
+  outputs[data.input].text(data.pressed);
 });
